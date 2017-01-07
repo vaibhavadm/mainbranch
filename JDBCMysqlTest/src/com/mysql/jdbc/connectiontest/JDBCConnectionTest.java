a402c594a546225a88970010fd7d67cd098ade3f
vaibhavadm <vaibhavreddy455@gmail.com>
2017-01-07 15:11:22 -0500

    first commit on JDBC project

---
+++ JDBCMysqlTest/src/com/mysql/jdbc/connectiontest/JDBCConnectionTest.java
@@ -0,0 +1,39 @@
+package com.mysql.jdbc.connectiontest;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import com.mysql.jdbc.Connection;
+
+public class JDBCConnectionTest {
+	public static void main(String[] args) {
+		System.out.println("-------- MySQL JDBC Connection Testing ------------");
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			System.out.println("Where is your MySQL JDBC Driver?");
+			e.printStackTrace();
+			return;
+		}
+
+		System.out.println("MySQL JDBC Driver Registered!");
+		Connection connection = null;
+
+		try {
+			connection = (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/us_states?useSSL=false", "root",
+					"0447");
+
+		} catch (SQLException e) {
+			System.out.println("Connection Failed! Check output console");
+			e.printStackTrace();
+			return;
+		}
+
+		if (connection != null) {
+			System.out.println("You made it, take control your database now!");
+		} else {
+			System.out.println("Failed to make connection!");
+		}
+	}
+}
