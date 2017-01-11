a9bf59af48584be3cab963c3b70c70a8e707737c
vaibhavadm <vaibhavreddy455@gmail.com>
2017-01-11 14:43:10 -0500

    adding the traditional method
    Another way to reverse the String in java without using API

--- HelloWorld/src/com/git/testing/StringReverse.java
+++ HelloWorld/src/com/git/testing/StringReverse.java
@@ -5,5 +5,22 @@ public static void main(String[] args) {
 	String givenString = "abcdefgh";
 	StringBuffer buffer = new StringBuffer(givenString);
 	System.out.println(buffer.reverse());
+	
+	// Using traditional method to reverse the string below
+	
+	//String givenString = "ABCDEFGH";
+	//char[] intoChars = givenString.toCharArray();
+	//char[] finalChars = null;
+	//System.out.println(intoChars);
+	//int countofChars = intoChars.length;
+	//for(int i=0; i<intoChars.length; i++){
+	//int temp = countofChars-1;
+	//char tempChar = intoChars[temp];
+	//System.out.print(tempChar);
+	//countofChars--;
+	
+	
+	
+		
 }
 }
