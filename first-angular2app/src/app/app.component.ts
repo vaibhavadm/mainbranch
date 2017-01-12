ffd5338c8b07cf161ca725d0ba323ac275a2fb89
vaibhavadm <vaibhavreddy455@gmail.com>
2017-01-11 23:26:48 -0500

    adding new component in angular2

--- first-angular2app/src/app/app.component.ts
+++ first-angular2app/src/app/app.component.ts
@@ -2,9 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+  		<h1> Inline Template </h1>
+  		<fa-other></fa-other>
+      <fa-another></fa-another>
+  `,
+  styles: [`
+  		h1 {
+  			color:red;
+  		}
+  `]
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Edited my First Angular-2 app!';
 }
