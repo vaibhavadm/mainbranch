693d65765f0b028a85e390ae2ebe5ac276eff867
vaibhavadm <vaibhavreddy455@gmail.com>
2017-01-13 10:13:57 -0500

    Adding the complete code for the snake project

--- SnakeGame/main.js
+++ SnakeGame/main.js
@@ -1,44 +1,166 @@
 $(document).ready(function(){
-	//Canvas stuff
-	var canvas = $("#canvas")[0];// same as document.getElementById('canvas') in vanilla JS.
-	// returns the DOM node, which has access to DOM methods, but not jQuery methods.
-
-	var ctx = canvas.getContext("2d");// The getContext() method is default methd in HTMLObject 
-	//that provide properties and methods for drawing.
+	// Same as document.getElementById('canvas')in vanilla JS.
+	// returns the DOM node, which has access to DOM methods, but not jQuery
+	// methods.
+	var canvas = $("#canvas")[0];
+    
+    // The getContext() method is default method in HTMLObject
+	// that provide properties and methods for drawing.
+	var ctx = canvas.getContext("2d");
+    
+    // gets the width of the canvas object defined in HTML File i.e. 450
+	var w = $("#canvas").width();
+    
+    // gets the height of the canvas object defined in HTML File i.e. 450
+	var h = $("#canvas").height();
 	
-	var w = $("#canvas").width(); // gets the width of the canvas object defined in HTML File i.e. 200
-	var h = $("#canvas").height(); // gets the height of the canvas object defined in HTML File i.e. 100
-	
-	//Lets paint the canvas now
-	ctx.fillStyle= "white"; // fill the canvas gadiant with white color
-
-	//giving the rectangle a Gradient stroke and setting the same dimentions for alignment 
-	var gradient = ctx.createLinearGradient(0, 0, w, h);
-	//adding colors to gradient
-	gradient.addColorStop("0", "magenta");
-	gradient.addColorStop("0.5", "blue");
-	gradient.addColorStop("1.0", "red");
-
-	//removing this because of adding Gradient
-	//ctx.fillRect(0, 0, w, h);
-	 // referes to context.fillRect(x,y,width,height);
-	// x being the X co-ordinate of the rectangle's upper-left corner here 0
-	// y being the y co-ordinate of the rectangle's upper-left corner here 0
-	// Width of the rectangle in pixels
-	// Height of the rectangle in pixels
-
-	//Assign Gradient to the context stroke adding color
-	ctx.strokeStyle = gradient; 
-
-	ctx.strokeRect(0, 0, w, h);
+	//Lets save the cell width in a variable for easy control
+	var cw = 10;
+    //default direction the snake has to move.
+	var d;
+	var food;
+	var score;
 	
 	//Lets create the snake now
 	var snake_array; //an array of cells to make up the snake
+	
+	function init(){
+		d = "right";
+		create_snake();
+		//Now we can see the food particle
+       		create_food(); 
+		//finally lets display the score
+		score = 0;
+		//if the variable game_loop is undefined, clear the exisiting interval whose id is game_loop
+		//call paint every 80 milliseconds and store the interval id in game_loop.
+		if(typeof game_loop != "undefined") clearInterval(game_loop);
+		game_loop = setInterval(paint, 80);
+	}
+	init();
 	
 	function create_snake()
 	{
 		var length = 5; //Length of the snake
 		snake_array = []; //Empty array to start with
-		for(var i = length-1; 
+		for(var i = length-1; i>=0; i--)
+		{
+			//This will create a horizontal snake starting from the top left
+			snake_array.push({x: i, y:0});
+		}
 	}
-})
+	
+	//Lets create the food now
+	function create_food(){
+		food = {
+			x: Math.round(Math.random()*(w-cw)/cw), 
+			y: Math.round(Math.random()*(h-cw)/cw), 
+		};
+		//This will create a cell with x/y between 0-44
+		//Because there are 45(450/10) positions accross the rows and columns
+	}
+	
+	//Lets paint the snake now
+	function paint(){
+		//To avoid the snake trail we need to paint the BG on every frame
+		//Lets paint the canvas now
+        var  gradient = ctx.createLinearGradient(20, 20, w, h);
+        gradient.addColorStop("0", "magenta");
+		gradient.addColorStop("0.5", "blue");
+		gradient.addColorStop("0", "red");
+        gradient.addColorStop("1.0", "black");
+        
+        
+		ctx.fillStyle = "white";
+		ctx.fillRect(0, 0, w, h);
+		ctx.strokeStyle = gradient;
+		ctx.strokeRect(0, 0, w, h);
+		//The movement code for the snake to come here.
+		//The logic is simple
+		//Pop out the tail cell and place it infront of the head cell
+		var nx = snake_array[0].x;
+		var ny = snake_array[0].y;
+		//These were the position of the head cell.
+		//We will increment it to get the new head position
+		//Lets add proper direction based movement now
+		if(d == "right") nx++;
+		else if(d == "left") nx--;
+		else if(d == "up") ny--;
+		else if(d == "down") ny++;
+		
+		//Lets add the game over clauses now
+		//This will restart the game if the snake hits the wall
+		//Lets add the code for body collision
+		//Now if the head of the snake bumps into its body, the game will restart
+		if(nx == -1 || nx == w/cw || ny == -1 || ny == h/cw || check_collision(nx, ny, snake_array))
+		{
+			//restart game
+			init();
+			//Lets organize the code a bit now.
+			return;
+		}
+		
+		//Lets write the code to make the snake eat the food
+		//The logic is simple
+		//If the new head position matches with that of the food,
+		//Create a new head instead of moving the tail
+		if(nx == food.x && ny == food.y)
+		{
+			var tail = {x: nx, y: ny};
+			score++;
+			//Create new food
+			create_food();
+		}
+		else
+		{
+			var tail = snake_array.pop(); //pops out the last cell
+			tail.x = nx; tail.y = ny;
+		}
+		//The snake can now eat the food.
+		
+		snake_array.unshift(tail); //puts back the tail as the first cell
+		
+		for(var i = 0; i < snake_array.length; i++)
+		{
+			var c = snake_array[i];
+			//Lets paint 10px wide cells
+			paint_cell(c.x, c.y);
+		}
+		
+		//Lets paint the food
+		paint_cell(food.x, food.y);
+		//Lets paint the score
+		var score_text = "Score: " + score;
+		ctx.fillText(score_text, 5, h-5);
+	}
+	
+	//Lets first create a generic function to paint cells
+	function paint_cell(x, y){
+		ctx.fillStyle = "blue";
+		ctx.fillRect(x*cw, y*cw, cw, cw);
+		ctx.strokeStyle = "white";
+		ctx.strokeRect(x*cw, y*cw, cw, cw);
+	}
+	
+	function check_collision(x, y, array){
+		//This function will check if the provided x/y coordinates exist
+		//in an array of cells or not
+		for(var i = 0; i < array.length; i++)
+		{
+			if(array[i].x == x && array[i].y == y)
+			 return true;
+		}
+		return false;
+	}
+	
+	//Lets add the keyboard controls now
+	$(document).keydown(function(e){
+		var key = e.which;
+		//We will add another clause to prevent reverse gear
+		if(key == "37" && d != "right") d = "left";
+		else if(key == "38" && d != "down") d = "up";
+		else if(key == "39" && d != "left") d = "right";
+		else if(key == "40" && d != "up") d = "down";
+		//The snake is now keyboard controllable
+	})
+	
+});
