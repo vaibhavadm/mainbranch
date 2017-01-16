9ca46842f3204a0d5f6b9e9af6f2ef083a0235b1
vaibhavadm <vaibhavreddy455@gmail.com>
2017-01-16 14:46:46 -0500

    adding the initial Angular2 app

---
+++ recipe-book/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import {HeaderComponent} from './header.component';
+import {RecipesComponent} from './recipes/recipes.component';
+
+@Component({
+  selector: 'rb-root',
+  templateUrl: './app.component.html'
+})
+export class AppComponent {
+}
---
+++ recipe-book/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+
+import { Recipe } from '../recipe';
+
+@Component({
+	selector: 'rb-recipe-list',
+	templateUrl: './recipe-list.component.html'
+})
+export class RecipeListComponent implements OnInit {
+
+	recipes: Recipe[] = [];
+	@Output() recipeSelected = new EventEmitter<Recipe>();
+	recipe=  new Recipe('Dummy', 'Dummy','https://upload.wikimedia.org/wikipedia/commons/b/b5/Cumulus_clouds_in_fair_weather.jpeg');
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	onSelected(recipe: Recipe){
+		this.recipeSelected.emit(recipe);
+	}
+
+}
