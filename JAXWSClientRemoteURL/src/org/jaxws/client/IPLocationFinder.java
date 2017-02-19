c6952bbc05353740a1c68d5b6edc6b64a7a41c61
vaibhavadm <vaibhavreddy455@gmail.com>
2017-02-19 16:40:36 -0500

    Simple JAXWS client for remote URL

---
+++ JAXWSClientRemoteURL/src/org/jaxws/client/IPLocationFinder.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.jaxws.client;
+
+import net.webservicex.GeoIP;
+import net.webservicex.GeoIPService;
+import net.webservicex.GeoIPServiceSoap;
+
+/**
+ * @author VAIBHAVREDDYGUDDETI Feb 19, 2017
+ * We can generate a stub with the wsimport command from command line 
+ * http://www.webservicex.net/geoipservice.asmx?WSDL 
+ * wsimport -keep -s src http://www.webservicex.net/geoipservice.asmx?WSDL 
+ */
+public class IPLocationFinder {
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("You need to pass a IP address");
+		} else {
+			String ipAddress = args[0];
+			GeoIPService ipService = new GeoIPService();
+			GeoIPServiceSoap stub = ipService.getGeoIPServiceSoap();
+			GeoIP geoIP = stub.getGeoIP(ipAddress);
+			System.out.println(geoIP.getCountryName());
+		}
+	}
+}
