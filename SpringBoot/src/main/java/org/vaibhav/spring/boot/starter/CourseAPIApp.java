e40144c59b4172ac9ffbfaff41581b17df2a2dc8
vaibhavadm <vaibhavreddy455@gmail.com>
2017-04-01 23:33:04 -0400

    adding spring boot app setup

---
+++ SpringBoot/src/main/java/org/vaibhav/spring/boot/starter/CourseAPIApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package org.vaibhav.spring.boot.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author VAIBHAVREDDYGUDDETI
+ *Apr 1, 2017
+ */
+@SpringBootApplication
+public class CourseAPIApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CourseAPIApp.class, args);
+	}
+
+}
