e857fcc65693a65d534ecccd5203c89e35a9a349
vaibhavadm <vaibhavreddy455@gmail.com>
2017-04-02 23:24:51 -0400

    adding new method in resource class

--- SpringBoot/src/main/java/org/vaibhav/spring/boot/starter/topic/TopicController.java
+++ SpringBoot/src/main/java/org/vaibhav/spring/boot/starter/topic/TopicController.java
@@ -6,6 +6,7 @@ package org.vaibhav.spring.boot.starter.topic;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.vaibhav.spring.boot.starter.topic.service.TopicService;
@@ -24,5 +25,11 @@ public class TopicController {
 	@RequestMapping("/topics")
 	public List<Topic> getAllTopics(){
 		return topicService.getAllTopics();
+	}
+	
+	//same as JAX-RS way of doing in jaxrs we use PathParam here we use PathVariable
+	@RequestMapping("/topics/{id}")
+	public Topic getSingleTopic(@PathVariable(value = "id") String id){
+		return topicService.getTopic(id);
 	}
 }
