858c753556ca37b9be074dad1376f941fee2ea51
vaibhavadm <vaibhavreddy455@gmail.com>
2017-04-16 00:40:43 -0400

    adding anonymous inner classes

--- Java8Lambdas/src/com/vaibhav/after8/Greeter.java
+++ Java8Lambdas/src/com/vaibhav/after8/Greeter.java
@@ -6,9 +6,25 @@ package com.vaibhav.after8;
 import com.vaibhav.before8.IGreeting;
 
 /**
- * @author VAIBHAVREDDYGUDDETI
- *Apr 12, 2017
+ * @author VAIBHAVREDDYGUDDETI Apr 12, 2017
  */
 public class Greeter {
-	bolckOfCodeFunction = () ->  System.out.print("Hello World from java-8");
+
+	public static void main(String[] args) {
+		//using lamda we dont have to create a implementation class again
+		IGreeting blockOfCodeFunction = () -> System.out.print("Hello World from java-8");
+		blockOfCodeFunction.greet();
+		
+		//Anonymous Inner class is a class inside an another class with out a name "anonymous"
+		IGreeting ananymousInnerClass = new IGreeting() {
+			
+			@Override
+			public void greet() {
+				System.out.println("\n"+ "Hello World from Anonymous Inner class.");
+				
+			}
+		};
+		
+		ananymousInnerClass.greet();
+	}
 }
