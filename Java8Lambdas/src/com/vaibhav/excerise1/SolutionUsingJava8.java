0aa6465a171a4a363a877e453836f709e6a66f1c
vaibhavadm <vaibhavreddy455@gmail.com>
2017-04-17 22:32:51 -0400

    adding more Functional Interfaces

--- Java8Lambdas/src/com/vaibhav/excerise1/SolutionUsingJava8.java
+++ Java8Lambdas/src/com/vaibhav/excerise1/SolutionUsingJava8.java
@@ -6,6 +6,8 @@ package com.vaibhav.excerise1;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
 /**
  * @author VAIBHAVREDDYGUDDETI
@@ -30,27 +32,29 @@ public class SolutionUsingJava8 {
 		System.out.println("\n" + "---> Doing the printALL operation---->");
 		//printAll(people);
 		//for better code optimization use the below line instead of the above
-		PrintAllWithCondition(people, p -> true);
+		PrintAllWithCondition(people, p -> true, p -> System.out.println(p));
 		
 		//Step-3: Create a list of all people that has their "last name" starting with c
 		System.out.println("\n" + "---> Doing the PrintAllLastNameBeginingWithC operation---->");
-		PrintAllWithCondition(people, p -> p.getLastName().startsWith("C"));
+		PrintAllWithCondition(people, p -> p.getLastName().startsWith("C"), p -> System.out.println(p));
 		
 		// Step-4: Create a list of all people that has their "first name"
 		// starting with c-- this time pass the behavior to the method
 		System.out.println("\n" + "---> Doing the PrintAllFisrttNameBeginingWithC operation---->");		
-		PrintAllWithCondition(people, p -> p.getFirstName().startsWith("C"));		
+		PrintAllWithCondition(people, p -> p.getFirstName().startsWith("C"), p -> System.out.println(p.getFirstName()));		
 	}
 
 
 	/**
 	 * @param people
 	 * @param condition, already defined in previous class
+	 * Using Consumer interface to pass the behaviour instead of printing sysout inside
 	 */
-	private static void PrintAllWithCondition(List<Person> people, Condition condition) {
+	private static void PrintAllWithCondition(List<Person> people, Predicate<Person> prediacte, Consumer<Person> consumer) {
 		for(Person p: people){
-			if(condition.test(p)){
-				System.out.println(p);
+			if(prediacte.test(p)){
+				//System.out.println(p);	
+				consumer.accept(p);
 			}
 		}
 		
